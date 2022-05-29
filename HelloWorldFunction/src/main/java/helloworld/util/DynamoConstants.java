8a5d1570b6ff43cca9b63162c5ffba0639353043
andre <adozoo.c@gmail.com>
2022-05-29 22:09:35 +0900

    002

---
+++ HelloWorldFunction/src/main/java/helloworld/util/DynamoConstants.java
@@ -0,0 +1,15 @@
+package helloworld.util;
+
+public class DynamoConstants {
+    public static final String PARTITION_KEY = "janCode";
+
+    public static final String SORT_KEY = "rank";
+
+    public static final String TABLE_NAME = "cainzPoToi";
+
+    public static final String REGION = "ap-northeast-1";
+
+    public static final String ACCESS_KEY = "AKIAU5S4KCPJARQN23XM";
+
+    public static final String ACCESS_SECRET = "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";
+}
